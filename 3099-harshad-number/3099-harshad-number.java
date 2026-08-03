5fc414fc0acfd1c11f51277e4391a6681fa30153
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-08-03 11:09:38 +0530

    Time: 0 ms (100%), Space: 42.3 MB (24.67%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.java
+++ 3099-harshad-number/3099-harshad-number.java
@@ -1,17 +1,15 @@
 class Solution {
     public int sumOfTheDigitsOfHarshadNumber(int x) {
-        int n = x;
-        int sum = 0;
-        while (n!=0){
-            int dig = n%10;
-            sum += dig;
-            n = n/10;
-        }
-        if (x%sum == 0){
-            return sum;
-        }
-        else {
-            return -1;
-        }
+       String s = String.valueOf(x);
+       int sum = 0;
+       for (int i = 0;i<s.length();i++){
+        sum += s.charAt(i)-'0';
+       }
+       if (x%sum == 0){
+        return sum;
+       }
+       else {
+        return -1;
+       }
     }
 }
