1a39667e61633221ffec9dcd86e11413d1f78b17
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-08-07 22:12:17 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,8 +3,12 @@ class Solution {
         if (n<=1){
             return n;
         }
-        else {
-            return fib(n-1)+fib(n-2);
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
+        return dp[n];
     }
 }
