367dc1f24fa1bd3b31b64636852a07ceab169333
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-08-09 15:16:48 +0530

    Time: 0 ms (100%), Space: 42.4 MB (88.61%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,21 +1,13 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int sum[] = new int[1];
-        List<Integer> list = new ArrayList<>();
-        backtrack(nums,0,list,sum);
-        return sum[0];
+        return backtrack(nums,0,0);
     }
-    private void backtrack(int nums[],int pos,List<Integer> arr,int sum[]){
-        int xor = 0;
+    private int backtrack(int nums[],int pos,int xor){
         int n = nums.length;
-        for (int num:arr){
-           xor ^= num;
-        }
-        sum[0] += xor;
+        int sum = xor;
         for (int i = pos;i<n;i++){
-            arr.add(nums[i]);
-            backtrack(nums,i+1,arr,sum);
-            arr.remove(arr.size()-1);
+            sum += backtrack(nums,i+1,xor^nums[i]);
         }
+        return sum;
     }
 }
