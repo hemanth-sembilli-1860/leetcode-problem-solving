2abdb2b11b77bac250a0db45035a9806e04220de
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-09-04 11:15:56 +0530

    Time: 14 ms (92.35%), Space: 51.3 MB (87.49%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n = g.length;
+        int m = s.length;
+        int i = 0;
+        int j = 0;
+        int c = 0;
+        while (i<n && j<m){
+            if (s[j]>=g[i]){
+                i++;
+                j++;
+                c++;
+            }
+            else {
+                j++;
+            }
+        }
+        return c;
+    }
+}
