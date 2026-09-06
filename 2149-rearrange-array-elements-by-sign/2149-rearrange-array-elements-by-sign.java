b78ca136bb3d7cb5447a6c8346df791db12ba30e
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-09-06 12:12:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -3,16 +3,14 @@ class Solution {
         int n = nums.length;
         int c[] = new int[n];
         int j = 0;
+        int k = 1;
         for (int i = 0;i<n;i++){
             if (nums[i] > 0){
                 c[j] = nums[i];
                 j = j+2;
             }
-        }
-        int k = 1;
-        for (int i = 0;i<n;i++){
-            if (nums[i]<0){
-                c[k] = nums[i];
+            else {
+                 c[k] = nums[i];
                 k = k+2;
             }
         }
