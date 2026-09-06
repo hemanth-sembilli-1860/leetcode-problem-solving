c8d0f407968291ddc0f1c7492c841ffd08bbb84b
Hemanth Sembilli <hemanth.sembilli@gmail.com>
2026-09-06 10:03:16 +0530

    Time: 13 ms (93.74%), Space: 54.2 MB (73.01%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = t.length();
+        int m = s.length();
+        if (m<n){
+            return 0;
+        }
+        int dp[][] = new int[m+1][n+1];
+        for (int i = 0;i<=m;i++){
+            dp[i][n] = 1;
+        }
+        for (int i = m-1;i>=0;i--){
+            char c1 = s.charAt(i);
+            for (int j = n-1;j>=0;j--){
+                char c2 = t.charAt(j);
+                if (c1==c2){
+                    dp[i][j] = dp[i+1][j+1]+dp[i+1][j];
+                }
+                else {
+                    dp[i][j] = dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
